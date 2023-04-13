e544d92bde2699ab59a4a6b2828e7fa223663ab6
ShaminasP <mrzeddem@gmail.com>
2023-04-13 10:01:22 +0530

    changes made

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const connectDB = require("./Config/db");
 
@@ -7,7 +8,7 @@ const userRoute = require("./Routes/userRoute");
 const toDoRoute = require("./Routes/toDoRoute");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 connectDB();
 app.use("/", userRoute);
